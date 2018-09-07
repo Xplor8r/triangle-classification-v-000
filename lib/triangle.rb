a7dce83dbb235022d336de8506f94fdd309dab68
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-07 23:23:35 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,21 +7,25 @@ class Triangle
     @c = c
   end
   
+  def is_triangle?(a,b,c)
+    [a,b,c].max < [a,b,c].sum/2.0
+  end
+  
   def kind
-    if [a,b,c].max > [a,b,c].inject(:+)/2.0
+    if !is_triangle?
       begin
         raise TriangleError
       rescue TriangleError => error
         puts error.message
       end
+    end
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
     else
-      if a == b && b == c
-        :equilateral
-      elsif a == b || b == c || a == c
-        :isosceles
-      else
-        :scalene
-      end
+      :scalene
+    end
     end
   end
   
