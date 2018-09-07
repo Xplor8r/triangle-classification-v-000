649ddb8feaee74d019d875385c2c8afc09233033
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-07 23:08:05 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,7 @@ class Triangle
   end
   
   def kind
-    if a == 0 && b == 0 && c == 0
+    if [a,b,c].max > [a,b,c].sum/2.0
       begin
         raise TriangleError
       rescue TriangleError => error
